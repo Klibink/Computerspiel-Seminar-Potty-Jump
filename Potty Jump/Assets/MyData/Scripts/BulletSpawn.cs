f06ad11c6853f5d6b2c802626921d3974ab6ba9d
daniel_s <35691859+stemmerSoSe19@users.noreply.github.com>
2019-11-06 22:56:00 +0100

    Shooting implementiert
    Schusswinkel ändert sich nun je nach x-Position des Mausklicks oder Touch Inputs (Touch noch nicht getestet)

--- Potty Jump/Assets/MyData/Scripts/BulletSpawn.cs
+++ Potty Jump/Assets/MyData/Scripts/BulletSpawn.cs
@@ -5,23 +5,31 @@ using UnityEngine;
 public class BulletSpawn : MonoBehaviour
 {
     public GameObject bulletPrefab;
+    public float rot_range = 90f;
+    private float rot;
 
     // Update is called once per frame
     void Update()
     {
 #if UNITY_EDITOR
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetMouseButtonDown(0))
         {
             GameObject spawn = GameObject.Find("Spawn");
+            //Rotation des Spawn-Objects zwischen 45 und -45 Grad, lässt sich durch rot_range ändern
+            rot = -(rot_range * (Input.mousePosition.x / Screen.width) - (rot_range/2));
+            transform.rotation = Quaternion.Euler(0f, 0f, rot);
             Instantiate(bulletPrefab, spawn.transform.position, spawn.transform.rotation);
         }
 
 #elif UNITY_ANDROID
         //TODO: Touch input
 
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetTouch(0))
         {
+            Touch touch = Input.GetTouch(0)
             GameObject spawn = GameObject.Find("Spawn");
+            rot = -(rot_range * (touch.position.x / Screen.width) - (rot_range/2));
+            transform.rotation = Quaternion.Euler(0f, 0f, rot);
             Instantiate(bulletPrefab, spawn.transform.position, spawn.transform.rotation);
         }
 #endif
