e2ad3611733fa41a3d9e9de39bbb83bcb05c2bbf
Klibink <35121031+Klibink@users.noreply.github.com>
2019-12-15 15:11:23 +0100

    PottyObjekt im MainMenu für Skinauswahl eingefügt

---
+++ Potty Jump/Assets/MyData/Scripts/PottyMainMenu.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PottyMainMenu : MonoBehaviour
+{
+    private Transform[] allChildren;
+    public GameObject[] skins;
+    public GameObject[] flowers;
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        allChildren = transform.GetComponentsInChildren<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Beim Beginn der Szene wird der gewünschte Skin in der Hierarchy aktivert
+        for (int i = 0; i < skins.Length; i++)
+        {
+            if (i == GameManager.instance.currentSkin)
+            {
+                skins[i].SetActive(true);
+            }
+            else
+            {
+                skins[i].SetActive(false);
+            }
+        }
+        //Das Gleiche für die Blume
+        for (int i = 0; i < flowers.Length; i++)
+        {
+            if (i == GameManager.instance.currentFlower)
+            {
+                flowers[i].SetActive(true);
+            }
+            else
+            {
+                flowers[i].SetActive(false);
+            }
+        }
+    }
+}
