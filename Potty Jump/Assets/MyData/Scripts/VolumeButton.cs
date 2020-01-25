0439e4a445828ed9d97a294e54eb72e5d00aee0d
daniel_s <35691859+stemmerSoSe19@users.noreply.github.com>
2020-01-25 17:12:47 +0100

    Mute Button Hauptmenü
    - Mute Button im Hauptmenü implementiert

---
+++ Potty Jump/Assets/MyData/Scripts/VolumeButton.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VolumeButton : MonoBehaviour
+{
+    public bool soundOn = true;
+    public GameObject camera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    public void ToggleAudio()
+    {
+        if (soundOn)
+        {
+            camera.GetComponent<AudioListener>().enabled = false;
+            soundOn = false;
+        }
+        else
+        {
+            camera.GetComponent<AudioListener>().enabled = true;
+            soundOn = true;
+        }
+    }
+}
