8411265162c5f264ff44e60569dbe8103fa17788
Klibink <35121031+Klibink@users.noreply.github.com>
2020-01-25 19:59:00 +0100

    Kleine Änderungen
    -Hauptmenü-Button in MainScene hat richtige Funktion
    -MainSceneUiManager-Script hinzugefügt, damit im letzten Level kein Wolken UI angezeigt wird

---
+++ Potty Jump/Assets/MyData/Scenes/Endless/MainSceneUIManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainSceneUIManager : MonoBehaviour
+{
+    public GameObject collectItemsImage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (GameManager.instance.currentLevel + 1 == GameManager.instance.maxLevel)
+        {
+            collectItemsImage.SetActive(false);
+        }
+        else
+        {
+            collectItemsImage.SetActive(true);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
